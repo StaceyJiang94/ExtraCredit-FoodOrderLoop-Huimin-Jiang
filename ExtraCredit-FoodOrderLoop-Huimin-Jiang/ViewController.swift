753260b392a49c45a3a1d4bf5ac21e6cc5d59126
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-03-26 13:00:14 -0700

    Extra credit- FoodOrderLoop

--- ExtraCredit-FoodOrderLoop-Huimin-Jiang/ViewController.swift
+++ ExtraCredit-FoodOrderLoop-Huimin-Jiang/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var foodImageView: UIImageView!
+    
+    @IBOutlet weak var foodNamePriceLabel: UILabel!
+    
+    @IBOutlet weak var foodOrderReceiptView: UITextView!
+    
+    let foodImageArray = ["chicken", "hamburger", "noodle", "pasta", "pizza", "salad", "steak"]
+    
+    let foodNameArray = ["Roasted Chicken", "Classic Burger", "Chicken Noodle Soup", "Bow-Tie Pasta", "Pepperoni Pizza", "Italian Salad", "Grilled Steak"]
+    
+    let foodPriceArray = [13.99, 13.99, 11.99, 12.99, 11.99, 10.99, 15.99]
+    
+    var foodIndex = 0
+    
+    var chosenFoodNameArray: [String] = []
+    
+    var chosenFoodPriceArray: [Double] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        foodImageView.image = UIImage(named: foodImageArray[foodIndex])
+        
+        foodNamePriceLabel.text = "\(foodNameArray[foodIndex]) : $ \(foodPriceArray[foodIndex])"
+        
+        foodOrderReceiptView.text = "Food Order in Your Cart:"
     }
 
+    
+    @IBAction func nextDishButtonClicked(_ sender: UIButton) {
+        
+        foodIndex += 1
+        if foodIndex == foodImageArray.count{
+            foodIndex = 0
+        }
+        
+        foodImageView.image = UIImage(named: foodImageArray[foodIndex])
+        
+        foodNamePriceLabel.text = "\(foodNameArray[foodIndex]) : $ \(foodPriceArray[foodIndex])"
+    }
+    
+    
+    @IBAction func addToCartButtonClicked(_ sender: UIButton) {
+        var totalCost = 0.0
+        var allFoodAdd = ""
+
+        chosenFoodNameArray.append(foodNameArray[foodIndex])
+
+        chosenFoodPriceArray.append(foodPriceArray[foodIndex])
+
+        for count in 0...chosenFoodNameArray.count-1{
+            allFoodAdd = allFoodAdd + "\n \(chosenFoodNameArray[count]) : $ \(chosenFoodPriceArray[count])"
+            totalCost = totalCost + chosenFoodPriceArray[count]
+        }
+            
+        print(allFoodAdd)
+        print(totalCost)
+
+        foodOrderReceiptView.text = "Food Order in Your Cart: \n\(allFoodAdd) \n\n Total Cost: \(totalCost)"
+        
+   }
 
 }
-
